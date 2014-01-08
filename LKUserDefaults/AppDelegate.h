9b40cf584b11ba0c1c2ce23ad1fd6b2c0afc3b91
Hiroshi Hashiguchi <hashiguchi@lakesoft.jp>
2014-01-08 12:25:57 +0900

    Initial Commit

---
+++ LKUserDefaults/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LKUserDefaults
+//
+//  Created by Hiroshi Hashiguchi on 2014/01/08.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LKUserDefaults/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LKUserDefaults
+//
+//  Created by Hiroshi Hashiguchi on 2014/01/08.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
